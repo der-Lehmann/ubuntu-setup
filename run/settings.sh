cdcd691a2ba1c5788fcb233a538673f9ffb4e3f6
larslehmann <larsh.lehmann64@gmail.com>
2024-12-18 19:12:36 +0100

    Set the default terminal.

--- run/settings.sh
+++ run/settings.sh
@@ -1,2 +1,6 @@
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 900
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-timeout 300
+
+# Set default terminal
+sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator $(which alacritty) 50
+sudo update-alternatives --set x-terminal-emulator $(which alacritty)
