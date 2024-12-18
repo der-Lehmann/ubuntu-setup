f03dd887d6b6e2cfb4b81a4dfac2b27075148da3
larslehmann <larsh.lehmann64@gmail.com>
2024-12-18 19:25:10 +0100

    Added vim as an application to be installed.

--- run/terminal/miscellaneous.sh
+++ run/terminal/miscellaneous.sh
@@ -1,3 +1,3 @@
 echo "Sonstige Tools installieren"
 
-sudo apt install -y xcape pulseaudio-utils nemo blueman dunst brightnessctl x11-xserver-utils arandr okular qimgv trimage video-downloader timeshift
+sudo apt install -y xcape pulseaudio-utils nemo blueman dunst brightnessctl x11-xserver-utils arandr okular qimgv trimage video-downloader timeshift vim
