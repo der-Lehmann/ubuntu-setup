9264ee828fd7f0b9713a30b586504c12632d7431
larslehmann <larsh.lehmann64@gmail.com>
2024-12-19 10:52:24 +0100

    Created script to install cursor. Added Icon to WhatsApp.

---
+++ run/desktop/cursor.sh
@@ -0,0 +1,23 @@
+echo "Cursor installieren"
+
+mkdir -p ~/.local/share/applications
+mkdir -p ~/.local/share/icons
+
+cp ./run/desktop/icons/cursor-icon.png ~/.local/share/icons/cursor-icon.png
+
+wget -O /usr/local/bin/cursor.AppImage "https://downloader.cursor.sh/linux/appImage/x64"
+chmod +x /usr/local/bin/cursor.AppImage 
+
+cat <<EOF >~/.local/share/applications/Cursor.desktop
+[Desktop Entry]
+Version=1.0
+Name=Cursor
+Comment=Cursor AI Code Editor
+Exec=/usr/local/bin/cursor.AppImage
+Terminal=false
+Type=Application
+Icon=/home/$USER/.local/share/icons/cursor-icon.png
+Categories=GTK;
+MimeType=text/html;text/xml;application/xhtml_xml;
+StartupNotify=true
+EOF
--- run/desktop/whatsapp.sh
+++ run/desktop/whatsapp.sh
@@ -1,6 +1,9 @@
 echo "WhatsApp installieren"
 
 mkdir -p ~/.local/share/applications
+mkdir -p ~/.local/share/icons
+
+cp ./run/desktop/icons/whatsapp-icon.png ~/.local/share/icons/whatsapp-icon.png
 
 cat <<EOF >~/.local/share/applications/WhatsApp.desktop
 [Desktop Entry]
@@ -10,6 +13,7 @@ Comment=WhatsApp Messenger
 Exec=google-chrome --app="https://web.whatsapp.com" --name=WhatsApp --class=Whatsapp
 Terminal=false
 Type=Application
+Icon=/home/$USER/.local/share/icons/whatsapp-icon.png
 Categories=GTK;
 MimeType=text/html;text/xml;application/xhtml_xml;
 StartupNotify=true
