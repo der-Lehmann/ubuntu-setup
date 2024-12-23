08905e1bc392b9e9f9ba910d9a856cd68656fb4f
Lars Lehmann <larsh.lehmann64@gmail.com>
2024-12-23 11:27:34 +0100

    Fixed an error where an intervention was necessary to confirm how to deal with duplicate file names.

--- run/terminal/fonts.sh
+++ run/terminal/fonts.sh
@@ -2,11 +2,15 @@ echo "Fonts installieren"
 
 cd /tmp
 sudo rm -rf /tmp/font
-mkdir font
+mkdir /tmp/font
+cd /tmp/font
+
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/JetBrainsMono.zip
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/Meslo.zip
-unzip /tmp/JetBrainsMono.zip -d /tmp/font
-unzip /tmp/Meslo.zip -d /tmp/font
+unzip -o /tmp/JetBrainsMono.zip -d /tmp/font
+unzip -o /tmp/Meslo.zip -d /tmp/font
+
 sudo cp -f /tmp/font/*.ttf /usr/local/share/fonts/
 sudo fc-cache -f -v
+sudo rm -rf /tmp/font
 cd -
