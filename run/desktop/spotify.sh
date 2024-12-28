553eba28551a61dd81d2e1eff2069e00bafbf44c
Lars Lehmann <larsh.lehmann64@gmail.com>
2024-12-28 11:27:58 +0100

    Changed the spotify installation to use snap.

--- run/desktop/spotify.sh
+++ run/desktop/spotify.sh
@@ -1,5 +1,3 @@
-curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+echo "Spotify installieren"
 
-sudo apt update
-sudo apt install -y spotify-client
+sudo snap install spotify
