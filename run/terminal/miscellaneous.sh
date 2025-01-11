e07639b9db23ef6d87914f0ebc6303291aeccb73
Lars Lehmann <larsh.lehmann64@gmail.com>
2025-01-11 08:52:38 +0100

    Removed nemo

--- run/terminal/miscellaneous.sh
+++ run/terminal/miscellaneous.sh
@@ -1,3 +1,3 @@
 echo "Sonstige Tools installieren"
 
-sudo apt install -y xcape pulseaudio-utils nemo blueman dunst brightnessctl x11-xserver-utils arandr okular qimgv trimage video-downloader timeshift vim
+sudo apt install -y xcape pulseaudio-utils blueman dunst brightnessctl x11-xserver-utils arandr okular qimgv trimage video-downloader timeshift vim
