bae21c6122685ea058ae24ee4763ef1a2ac0386b
Lars Lehmann <larsh.lehmann64@gmail.com>
2025-03-31 08:35:15 +0200

    Explicitly set the default file browser.

--- run/settings.sh
+++ run/settings.sh
@@ -4,3 +4,7 @@ gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-tim
 # Set default terminal
 sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator $(which alacritty) 50
 sudo update-alternatives --set x-terminal-emulator $(which alacritty)
+
+# Set nemo as the default file browser.
+# This prevents disk-usage-analyzer to be the default.
+xdg-mime default nemo.desktop inode/directory
