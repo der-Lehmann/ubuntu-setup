98088b4edfbff15a0791572dffd5b70fc740cfbb
Lars Lehmann <larsh.lehmann64@gmail.com>
2025-04-29 13:59:49 +0200

    Added the ext-gd extension to the php installation.

--- run/terminal/php.sh
+++ run/terminal/php.sh
@@ -2,7 +2,7 @@ echo "PHP installieren"
 
 sudo add-apt-repository -y ppa:ondrej/php
 sudo apt update
-sudo apt install -y php8.4 php8.4-{curl,apcu,intl,mbstring,opcache,pgsql,mysql,sqlite3,redis,xml,zip}
+sudo apt install -y php8.4 php8.4-{curl,apcu,intl,mbstring,opcache,pgsql,mysql,sqlite3,redis,xml,zip,gd}
 
 sudo php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 sudo php composer-setup.php --quiet && sudo mv composer.phar /usr/local/bin/composer
