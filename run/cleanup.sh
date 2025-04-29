7b2bedb8b30cecebcf3ac2c2e8a303a26e97552e
Lars Lehmann <larsh.lehmann64@gmail.com>
2025-04-29 14:24:19 +0200

    All packages related to the apache webserver are removed.

--- run/cleanup.sh
+++ run/cleanup.sh
@@ -1,3 +1,11 @@
 echo "Cleanup"
 
 sudo apt remove -y nautilus
+
+# Remove Apache and related packages
+sudo apt remove -y apache2 apache2-bin apache2-data apache2-utils
+sudo apt remove -y libapache2-mod-*
+sudo apt autoremove -y
+sudo apt purge -y apache2*
+sudo rm -rf /etc/apache2
+sudo rm -rf /var/lib/apache2
